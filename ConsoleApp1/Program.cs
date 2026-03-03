45c8ba539350f7b69a014e6bac51b8621bc4b98d
Gustav <gustavjohnsson2008@gmail.com>
2026-03-03 13:51:24 +0100

    drawing

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,34 @@
 ﻿using Raylib_cs;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+class program
+{
+    static void Main()
+    {
+        // Start
+    
+        Raylib.InitWindow(1200, 750, "Skyrush");
+        Raylib.SetTargetFPS(60);
+        Texture2D cloud = Raylib.LoadTexture(@"nuvem.png");
+        Vector2 Player = new Vector2(400, 225);
 
+        while (!Raylib.WindowShouldClose())
+        
+        {
+        // Input
+            if(Raylib.IsKeyPressed(KeyboardKey.H)) ; 
+            if(Raylib.IsKeyPressed(KeyboardKey.J)) ; 
+            if(Raylib.IsKeyPressed(KeyboardKey.K)) ; 
+            if(Raylib.IsKeyPressed(KeyboardKey.L)) ; 
+
+        //Drawing
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.SkyBlue);
+            Raylib.DrawTexture(cloud, 200, 100, Color.White);
+            
+            Raylib.EndDrawing();
+        
+        }
+        Raylib.CloseWindow();
+    }
+}
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+647d7860153cc6669ab99c27d0102d0aa5faf768")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
